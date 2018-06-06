e71a18bd3cda1530796e128dbad580b2831e60b8
ccabo1 <ccabo@seas.upenn.edu>
2018-06-05 22:00:40 -0700

    Adding amenities images

--- src/components/previews/PreviewListing.js
+++ src/components/previews/PreviewListing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text, Button } from 'react-native';
+import { TouchableHighlight, View, Image, Text, Button } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../styles/previews/preview';
 
@@ -11,25 +11,23 @@ const PreviewListing = ({ listing, navigation }) => {
   if (!listing) return null;
   const id = listing._id || listing.contentId;
   return (
-    <View style={styles.preview}>
-      <Image
-        source={{ uri: listing.image }}
-        resizeMode="cover"
-        style={styles.image}
-      />
-      <Text style={styles.title}>
-        {listing.title}
-      </Text>
-      <Stars rating={listing.rating} />
-      <Categories categories={listing.categories} />
-      <Text style={styles.subtitle}>
-        {listing.description}
-      </Text>
-      <Button
-        onPress={() => navigation.navigate('Listing', { id })}
-        title="View"
-      />
-    </View>
+    <TouchableHighlight onPress={() => navigation.navigate('Listing', { id })}>
+      <View style={styles.preview}>
+        <Image
+          source={{ uri: listing.image }}
+          resizeMode="cover"
+          style={styles.image}
+        />
+        <Text style={styles.title}>
+          {listing.title}
+        </Text>
+        <Stars rating={listing.rating} />
+        <Categories categories={listing.categories} />
+        <Text style={styles.subtitle}>
+          {listing.description}
+        </Text>
+      </View>
+    </TouchableHighlight>
   );
 };
 
